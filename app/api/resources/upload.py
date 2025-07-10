95cf244b8e82510beedf274350ba994b37752fa7
Do Nguyen Phuong <nguyenphuongdo22@gmail.com>
2025-07-10 15:02:14 +0700

    start fix train model

--- app/api/resources/upload.py
+++ app/api/resources/upload.py
@@ -3,10 +3,9 @@ from flask_restful import Resource, request
 from flask_jwt_extended import jwt_required
 from ...services.upload_service import UploadService
 from werkzeug.utils import secure_filename
-import os
 
 class UploadPcap(Resource):
-    @jwt_required()
+    # @jwt_required()
     def post(self):
         if 'file' not in request.files:
             return {"error": "No file provided"}, 400
@@ -21,9 +20,16 @@ class UploadPcap(Resource):
             service = UploadService()
             result = service.process_pcap(file, filename)
             return {
-                "message": f"File {filename} uploaded and converted successfully",
+                "message": f"File {filename} processed successfully",
                 "pcap_path": result["pcap_path"],
-                "csv_path": result["csv_path"]
+                "csv_path": result["csv_path"],
+                "prediction": result["prediction"],
+                "mse_threshold": result["mse_threshold"],
+                "mse_values": result["mse_values"],
+                "normal_percentage": result["normal_percentage"],
+                "abnormal_percentage": result["abnormal_percentage"],
+                "max_mse": result["max_mse"],
+                "mean_mse": result["mean_mse"]
             }, 200
         except Exception as e:
             return {"error": str(e)}, 500
